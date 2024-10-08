1a986a2d54e9ab68d1b7aea66eb0ab432f604d7a
Nayara Souza <nayara.souza@sanar.com>
2024-10-08 19:35:59 -0300

    adds adjacency-list dfs method

--- graphs/adjacency-list/AdjacencyList.js
+++ graphs/adjacency-list/AdjacencyList.js
@@ -27,6 +27,27 @@ class AdjacencyList {
   isEdge(fromNode, toNode) {
     return this.list[fromNode].includes(toNode);
   }
+
+  dfs(start) {
+    const result = [];
+    const visited = {};
+    const list = this.list;
+
+    (function dfsHelper(node) {
+      if (!node) return null;
+
+      visited[node] = true;
+      result.push(node);
+
+      list[node].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          dfsHelper(neighbor);
+        }
+      });
+    })(start);
+
+    return result;
+  }
 }
 
 module.exports = AdjacencyList;
